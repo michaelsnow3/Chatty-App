f7a49ccee517c1852f96996d6e712954b57de5a0
vagrant <mikesnow444@gmail.com>
2019-02-12 18:34:57 +0000

    app now defines a function updating state when new message is sent in ChatBar

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,49 @@ class App extends Component {
         }
       ]
     }
+
+    this.addMessage = this.addMessage.bind(this);
   }
-  
+
+  //method that adds input message to messages array in state
+  addMessage(username, content) {
+    const id = this.state.messages[this.state.messages.length - 1].id + 1;
+    
+    const newMessage = {
+      username: username,
+      content: content,
+      id: id
+    }
+
+    //add new message object to messages array and update currentUser in state
+    this.setState({
+      currentUser: username,
+      messages: [...this.state.messages, newMessage]
+    })
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
   render() {
+    console.log("redering <app />");
     return (
       <div>
         <nav className='navbar'>
           <a href='/' className='navbar-brand' >Chatty</a>
         </nav>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser} />
+        <ChatBar addMessage={this.addMessage} currentUser={this.state.currentUser} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
-function ChatBar({ currentUser }) {
+function ChatBar({ currentUser, addMessage }) {
+
+  function onEnter(event) {
+    
+    //check if enter was pressed
+    if(event.key === 'Enter'){
+      const content = event.target.value;
+      const username = event.target.previousSibling.value;
+
+      //clear input
+      event.target.value = '';
+
+      addMessage(username, content);
+    }
+  }
+
   return(
     <footer className='chatbar'>
       <input 
@@ -8,7 +23,11 @@ function ChatBar({ currentUser }) {
         placeholder="Your Name (Optional)" 
         defaultValue={currentUser.name} 
       />
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      <input 
+        className="chatbar-message" 
+        placeholder="Type a message and hit ENTER" 
+        onKeyPress={onEnter}
+      />
     </footer>
   );
 }
