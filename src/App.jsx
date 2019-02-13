d8e71a6ab66000e05e275441fb14753a2b472c29
vagrant <mikesnow444@gmail.com>
2019-02-13 18:12:58 +0000

    updated addmessage function to send users message to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,7 @@ class App extends Component {
 
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: 1
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: 2
-        }
-      ]
+      messages: []
     }
 
     this.addMessage = this.addMessage.bind(this);
@@ -28,20 +17,13 @@ class App extends Component {
   //method that adds input message to messages array in state
   addMessage(username, content) {
 
-    //add 1 to last message id to get new id
-    const id = this.state.messages[this.state.messages.length - 1].id + 1;
-    
     const newMessage = {
       username: username,
-      content: content,
-      id: id
+      content: content
     }
 
-    //add new message object to messages array and update currentUser in state
-    this.setState({
-      currentUser: username,
-      messages: [...this.state.messages, newMessage]
-    })
+    this.socket.send(JSON.stringify(newMessage));
+  
   }
 
   componentDidMount() {
